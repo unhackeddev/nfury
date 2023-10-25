02ec1cf7873c590ce817c3cebf71cfe3bc1e9ec1
Cristiano Rodrigues <crodrigues@Cristianos-MacBook-Pro.local>
2023-10-25 16:13:09 -0300

    Initial setup and implemented the run command

---
+++ src/NFury/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using NFury.Commands;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using Spectre.Console.Rendering;
+
+WriteCopyrigth();
+
+var app = new CommandApp();
+app.Configure(config =>
+{
+    config.AddCommand<RunCommand>("run")
+        .WithDescription("Execute the load test.");
+});
+
+return app.Run(args);
+
+
+static void WriteCopyrigth()
+{
+    AnsiConsole.Clear();
+    AnsiConsole.Write(
+        new FigletText("NFury")
+            .LeftJustified()
+            .Color(Color.Blue));
+
+    Grid grid = new();
+    grid.AddColumn();
+    grid.AddColumn();
+    
+    grid.AddRow(new IRenderable[]
+    {
+        new Text("Developed by: ", new Style(Color.Red, Color.Default)).LeftJustified(),
+        new Text("Unhacked, 2023", new Style(Color.Red, Color.Default)).LeftJustified()
+    });
+    grid.AddRow(new IRenderable[]
+    {
+        new Text("Version: ", new Style(Color.Red, Color.Default)).LeftJustified(),
+        new Text($"{Assembly.GetEntryAssembly()?.GetName().Version}", new Style(Color.Red, Color.Default)).LeftJustified()
+    });
+    AnsiConsole.Write(grid);
+    AnsiConsole.WriteLine();
+}
